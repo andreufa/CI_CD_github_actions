c2603c0f648d717bd3a3adef9a0e999d5aa21f50
Букреев Андрей Анатольевич <aa.bukreev@mri-progress.ru>
2025-05-23 16:12:49 +0300

    TestIncrementalScreen added

---
+++ app/src/androidTest/java/i/progress/tech/github_actions/TestIncrementalScreen.kt
@@ -0,0 +1,24 @@
+package i.progress.tech.github_actions
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import i.progress.tech.github_actions.ui.IncrementScreen
+import i.progress.tech.github_actions.ui.theme.Github_actionsTheme
+import org.junit.Rule
+import org.junit.Test
+
+class TestIncrementalScreen {
+    @get:Rule val composeTestRule = createComposeRule()
+
+    @Test
+    fun shouldShow2(){
+        composeTestRule.setContent {
+            Github_actionsTheme {
+                IncrementScreen()
+            }
+        }
+        composeTestRule.onNodeWithText("Plus").performClick()
+        composeTestRule.onNodeWithText("Value = 2").assertExists()
+    }
+}
