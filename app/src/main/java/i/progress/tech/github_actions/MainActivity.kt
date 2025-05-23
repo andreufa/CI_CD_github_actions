5ef935971488f9ae0169611fd53c05383f228c8d
Букреев Андрей Анатольевич <aa.bukreev@mri-progress.ru>
2025-05-23 15:25:03 +0300

    MainActivity.kt

--- app/src/main/java/i/progress/tech/github_actions/MainActivity.kt
+++ app/src/main/java/i/progress/tech/github_actions/MainActivity.kt
@@ -1,9 +1,11 @@
 package i.progress.tech.github_actions
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,19 +13,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import i.progress.tech.github_actions.ui.IncrementScreen
 import i.progress.tech.github_actions.ui.theme.Github_actionsTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Github_actionsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Column(Modifier.padding(top = 48.dp)){
+                        IncrementScreen()
+                    }
+
                 }
             }
         }
